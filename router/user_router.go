2ebbbe33e8d5662a010b12ea6dc4bdcb9bb5321e
aoge666 <1409050584@qq.com>
2024-12-15 21:55:33 +0800

    qq登录修订

--- router/user_router.go
+++ router/user_router.go
@@ -10,5 +10,5 @@ func UserRouter(r *gin.RouterGroup) {
 	app := api.App.UserApi
 	r.POST("user/send_email", middleware.CaptchaMiddleware, app.SendEmailView)
 	r.POST("user/email", middleware.CaptchaMiddleware, app.RegisterEmail)
-	r.POST("user/qq", app.SendEmailView)
+	r.POST("user/qq", app.QQLoginView)
 }
